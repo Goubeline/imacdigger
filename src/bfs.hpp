06527bbdc9a1bb80b81a0f6228b50eb0d675800c
Yohann LAI <>
2025-05-23 09:03:11 +0200

    Mise à jour de bfs.hpp

--- src/bfs.hpp
+++ src/bfs.hpp
@@ -4,6 +4,9 @@
 
 #include <vector>
 
-void bfs_flow_field(std::vector<std::vector<Bloc>>& map, int start_x, int start_y);
 bool inbound(int x, int y);
+float calculateAngle(std::vector<float> vec1, std::vector<float> vec2);
+void find_flow(std::vector<std::vector<int>>& distances, std::vector<std::vector<Bloc>>& map, std::pair<int, int> start, std::pair<int, int> current);
 std::vector<std::vector<int>> dist_vec(std::vector<std::vector<Bloc>>& map, int start_x, int start_y);
+void flow_field(std::vector<std::vector<int>>& distances, std::vector<std::vector<Bloc>>& map, std::pair<int, int> start);
+void bfs_flow_field(std::vector<std::vector<Bloc>>& map, int start_x, int start_y);
